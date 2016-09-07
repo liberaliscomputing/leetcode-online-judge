d9f0cbc3eae337592bb02bbfcda9e2f1f4c66119
liberaliscomputing <liberalis.computing@gmail.com>
2016-09-07 04:15:14 -0400

    minor change(s)

--- algorithms/hash_table/easy/intersection_of_two_arrays.py
+++ algorithms/hash_table/easy/intersection_of_two_arrays.py
@@ -8,7 +8,7 @@ Given two arrays, write a function to compute their intersection.
 #Example
 Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
 
-$Note
+#Note
 Each element in the result must be unique.
 The result can be in any order.
 
