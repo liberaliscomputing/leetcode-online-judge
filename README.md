a1971f3a5fcd112b639e82b76bdb557a60ce43c5
liberaliscomputing <liberalis.computing@gmail.com>
2016-12-12 14:29:12 -0500

    add a soluton to 197. Rising Temperature

--- README.md
+++ README.md
@@ -56,5 +56,6 @@
 + 181. [Employees Earning More Than Their Managers](https://github.com/liberaliscomputing/leetcode-online-judge/blob/master/database/easy/employees_earning_more_than_their_managers.sql)
 + 182. [Duplicate Emails](https://github.com/liberaliscomputing/leetcode-online-judge/blob/master/database/easy/duplicate_emails.sql)
 + 183. [Customers Who Never Order](https://github.com/liberaliscomputing/leetcode-online-judge/blob/master/database/easy/customers_who_never_order.sql)
++ 197. [Rising Temperature](https://github.com/liberaliscomputing/leetcode-online-judge/blob/master/database/easy/rising_temperature.sql)
 
 ##Shell
---
+++ database/easy/rising_temperature.sql
@@ -0,0 +1,29 @@
+/**
+#Description
+Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+
++---------+------------+------------------+
+| Id(INT) | Date(DATE) | Temperature(INT) |
++---------+------------+------------------+
+|       1 | 2015-01-01 |               10 |
+|       2 | 2015-01-02 |               25 |
+|       3 | 2015-01-03 |               20 |
+|       4 | 2015-01-04 |               30 |
++---------+------------+------------------+
+
+For example, return the following Ids for the above Weather table:
+
++----+
+| Id |
++----+
+|  2 |
+|  4 |
++----+
+ */
+
+# Write your MySQL query statement below
+SELECT w1.Id
+FROM Weather w1
+INNER JOIN Weather w2
+ON w1.Date = w2.Date + INTERVAL 1 DAY
+WHERE w1.Temperature > w2.Temperature
